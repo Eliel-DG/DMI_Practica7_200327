957113c1a9eda9619583d11fa944cec9130cf6c6
Yahir hernandez <yahirhn66@gmail.com>
2023-09-25 10:29:26 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/dmi_practica7_200327/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dmi_practica7_200327
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:dmi_practica7_200327/home.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+    theme: new ThemeData.dark(),
+  ));
+}
